f3c6e949ebbdff45dfeffbd134e42b94bb4c5f2e
huseyinakdag3500 <112476837+huseyinakdag3500@users.noreply.github.com>
2025-04-22 14:27:12 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -73,7 +73,7 @@ async function getCurrentlyPlaying(accessToken, isInitial = false) {
           });
           
           
-        },100);
+        },500);
       }
       nowPlayingDiv.style.display = 'block';
     }
